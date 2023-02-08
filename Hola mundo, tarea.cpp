22d4e21a299d6c56764adf7141c7cf404ea23bbb
Jordy111111 <124799549+Jordy111111@users.noreply.github.com>
2023-02-08 08:43:25 -0600

    Add files via upload

---
+++ Hola mundo, tarea.cpp
@@ -0,0 +1,19 @@
+// Hola mundo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <math.h>
+
+
+
+int main()
+{
+    std::cout << "Jordy Herbert Enrique Cotton Diaz!\n";
+    std::cout << "202308027!\n";
+    std::cout << "Para poder crar aplicaciones de utilidades!\n";
+    std::cout << "SOY EL MAS PERRON AQUI!\n";
+    return 0;
+}
+
+
